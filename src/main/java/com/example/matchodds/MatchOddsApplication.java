72236e97b992e3ecc40065c192190bc7bb8c5dc0
laioss <laios.stavros@gmail.com>
2021-09-21 21:27:52 +0300

    initial commit

---
+++ src/main/java/com/example/matchodds/MatchOddsApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchodds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchOddsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchOddsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matchodds/MatchOddsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchodds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchOddsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
